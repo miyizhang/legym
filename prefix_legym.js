5d46345b03d4f01f67e25cb933abcd2d2f6a350b
miyizhang <112915938+miyizhang@users.noreply.github.com>
2024-03-31 20:39:15 +0800

    Update prefix_legym.js

--- prefix_legym.js
+++ prefix_legym.js
@@ -1,24 +1,3 @@
-/*
-乐健修改跑步限制
-脚本版本： 1.0.3
-脚本作者：BlingCc
-更新时间：2024-03-03
-主页：blingcc.eu.org
-声明：此脚本仅供学习交流
-
-**************************
-
-QuantumultX:
-
-[rewrite_locala]
-^https?:\/\/cpes\.legym\.cn\/running\/app\/getRunningLimit url script-response-body https://raw.githubusercontent.com/BlingCc233/MITM_modules/main/prefix_legym.js
-
-[mitm] 
-
-hostname = cpes.legym.cn
-
-*******************************/
-
 var obj = JSON.parse($response.body);
 
 obj.data.averageSpeedStart = "0";
